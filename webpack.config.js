ec27ddae76551f55b246bd43cd9c007a1dec45b8
rudzainy <rudzainy@gmail.com>
2017-06-29 10:32:36 +0800

    First build

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const { resolve } = require('path')
+
+const srcDir = resolve(__dirname, 'src')
+
+module.exports = {
+  entry: `${srcDir}/index.js`,
+  output: {
+    filename: 'bundle.js'
+  }
+}
